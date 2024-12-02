c6d00cc3b9bd254f852c341c1ad3bed5a28df44f
Marton Cserbak <mcs@penneo.com>
2024-12-02 10:14:49 +0100

    feat: Day2 part2

--- src/test/kotlin/org/cserby/aoc2024/test/Day2Test.kt
+++ src/test/kotlin/org/cserby/aoc2024/test/Day2Test.kt
@@ -13,4 +13,19 @@ class Day2Test {
     fun part1() {
         assertEquals(490, Day2.part1(Utils.readFile("/day2.input.txt")))
     }
+
+    @Test
+    fun part2example() {
+        assertEquals(4, Day2.part2(Utils.readFile("/day2.example.txt")))
+    }
+
+    @Test
+    fun part2debug() {
+        assertEquals(0, Day2.part2("1 2 7 8 9"))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(536, Day2.part2(Utils.readFile("/day2.input.txt")))
+    }
 }
