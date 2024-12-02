3a7551a07333f47cc6ba5d54f8c3eb7b784cf947
Marton Cserbak <mcs@penneo.com>
2024-12-02 10:14:49 +0100

    feat: Day2 part2

--- src/main/kotlin/org/cserby/aoc2024/Day2.kt
+++ src/main/kotlin/org/cserby/aoc2024/Day2.kt
@@ -11,12 +11,15 @@ object Day2 {
         return line.split(" ").map(String::toInt)
     }
 
+    private fun checkDiff(meas1: Int, meas2: Int): Boolean {
+        val diff = meas2 - meas1
+        return diff >= 1 && diff <= 3
+    }
+
     private fun isSafelyIncreasing(measurement: Measurement): Boolean {
         return measurement.zipWithNext().fold(true) {
             safeSoFar, currentPair ->
-                safeSoFar &&
-                        currentPair.second - currentPair.first >= 1 &&
-                        currentPair.second - currentPair.first <= 3
+                safeSoFar && checkDiff(currentPair.first, currentPair.second)
         }
     }
 
@@ -26,5 +29,25 @@ object Day2 {
 
     fun part1(input: String): Int {
         return parse(input).count(Day2::isMeasurementSafe)
+    }
+
+    private fun <T>removeIndex(list: List<T>, index: Int): List<T> {
+        return list.filterIndexed { idx, _ -> index != idx }
+    }
+
+    private fun increasingWithSkip(measurement: Measurement): Boolean {
+        if (isSafelyIncreasing(measurement)) return true
+        for (i in 0..measurement.size) {
+            if (isSafelyIncreasing(removeIndex(measurement, i))) return true
+        }
+        return false
+    }
+
+    private fun safeWithSkip(measurement: Measurement): Boolean {
+        return increasingWithSkip(measurement) || increasingWithSkip(measurement.reversed())
+    }
+
+    fun part2(input: String): Int {
+        return parse(input).filter(Day2::safeWithSkip).count()
     }
 }
