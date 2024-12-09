3792a158f5f9d66560514f632a86a8f5a4d01f21
Marton Cserbak <mcs@penneo.com>
2024-12-09 20:45:09 +0100

    feat: Day9 part2 (not proud)

--- src/main/kotlin/org/cserby/aoc2024/Day9.kt
+++ src/main/kotlin/org/cserby/aoc2024/Day9.kt
@@ -1,5 +1,7 @@
 package org.cserby.aoc2024
 
+data class File(val index: Int, val size: Int)
+
 object Day9 {
     private fun parse(input: String): Pair<List<Int>, List<Int>> {
         return input.toCharArray()
@@ -48,7 +50,69 @@ object Day9 {
         return checksum(fileMap)
     }
 
+    private fun parse2(input: String): List<File> {
+        return input.toCharArray()
+            .map { it.digitToInt() }
+            .foldIndexed(emptyList<File>()) { index, files, value ->
+                if (index % 2 == 0) files + File(index / 2, value) else files + File(-1, value)
+            }
+    }
+
+    private fun tryMove(
+        fileMap: MutableList<File>,
+        fileIdxToMove: Int,
+    ): MutableList<File> {
+        val idxOfFileToMove = fileMap.indexOfFirst { it.index == fileIdxToMove }
+        val fileToMove = fileMap[idxOfFileToMove]
+        val freeToUseIdx = fileMap.indexOfFirst { it.index == -1 && it.size >= fileToMove.size }
+        if (freeToUseIdx != -1 && freeToUseIdx < idxOfFileToMove) {
+            var freeToAdd = fileToMove.size
+            if (idxOfFileToMove < fileMap.size - 1 && fileMap[idxOfFileToMove + 1].index == -1) {
+                freeToAdd += fileMap[idxOfFileToMove + 1].size
+                fileMap.removeAt(idxOfFileToMove + 1)
+            }
+            fileMap.set(idxOfFileToMove, File(-1, freeToAdd))
+            if (idxOfFileToMove > 0 && fileMap[idxOfFileToMove - 1].index == -1) {
+                freeToAdd += fileMap[idxOfFileToMove - 1].size
+                fileMap.set(idxOfFileToMove, File(-1, freeToAdd))
+                fileMap.removeAt(idxOfFileToMove - 1)
+            }
+            val freeLeft = fileMap[freeToUseIdx].size - fileToMove.size
+            if (freeLeft > 0) {
+                fileMap.set(freeToUseIdx, File(-1, freeLeft))
+            } else {
+                fileMap.removeAt(freeToUseIdx)
+            }
+            fileMap.add(freeToUseIdx, fileToMove)
+            return fileMap
+        } else {
+            return fileMap
+        }
+    }
+
+    private fun defragged2(fileMap: List<File>): List<File> {
+        val fileCount = fileMap.count { it.index != -1 }
+        var mutableFileMap = fileMap.toMutableList()
+
+        for (fileIdxToMove in fileCount - 1 downTo 0) {
+            mutableFileMap = tryMove(mutableFileMap, fileIdxToMove)
+        }
+        return mutableFileMap
+    }
+
+    private fun chunks(fileMap: List<File>): Sequence<Int> =
+        sequence {
+            for (file in fileMap) {
+                yieldAll(fileContents(if (file.index == -1) 0 else file.index, file.size))
+            }
+        }
+
     fun part2(input: String): Long {
-        return 43
+        val fileMap = parse2(input)
+
+        return chunks(defragged2(fileMap)).foldIndexed(0L) {
+                index, acc, value ->
+            acc + index * value
+        }
     }
 }
