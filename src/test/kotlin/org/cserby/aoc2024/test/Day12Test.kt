16e00abd374960fd05c69cb94dcfb2cc1a6486d5
Marton Cserbak <mcs@penneo.com>
2024-12-12 09:29:27 +0100

    feat: Day12 part1

---
+++ src/test/kotlin/org/cserby/aoc2024/test/Day12Test.kt
@@ -0,0 +1,22 @@
+package org.cserby.aoc2024.test
+
+import org.cserby.aoc2024.day12.Day12
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day12Test {
+    @Test
+    fun part1example() {
+        assertEquals(1930, Day12.part1(Utils.readFile("/day12.example.txt")))
+    }
+
+    @Test
+    fun part1() {
+        assertEquals(216042, Day12.part1(Utils.readFile("/day12.input.txt")))
+    }
+
+    @Test
+    fun part2() {
+        //assertEquals(255758646442399L, Day12.part2(Utils.readFile("/day12.input.txt")))
+    }
+}
