fe3a14421a4754b023b6b9c6d89b1712a505801d
Marton Cserbak <mcs@penneo.com>
2024-12-18 13:40:41 +0100

    feat(WIP): Day17 part2

--- src/test/kotlin/org/cserby/aoc2024/test/Day17Test.kt
+++ src/test/kotlin/org/cserby/aoc2024/test/Day17Test.kt
@@ -14,4 +14,19 @@ class Day17Test {
     fun part1() {
         assertEquals("1,5,0,5,2,0,1,3,5", Day17.part1(Utils.readFile("/day17.input.txt")))
     }
+
+    @Test
+    fun part2part1() {
+        assertEquals("1,5,0,5,2,0,1,3,5", Day17.computer2(45483412).joinToString(","))
+    }
+
+    @Test
+    fun part2example() {
+        assertEquals(117440, Day17.part2(Utils.readFile("/day17.example2.txt")))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(5, Day17.part2(Utils.readFile("/day17.input.txt")))
+    }
 }
