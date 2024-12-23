b6ef0a291638c239d5d18f88031e54ebc191a516
Marton Cserbak <mcs@penneo.com>
2024-12-23 08:34:37 +0100

    feat: Day23 part2

--- src/main/kotlin/org/cserby/aoc2024/Day23.kt
+++ src/main/kotlin/org/cserby/aoc2024/Day23.kt
@@ -27,7 +27,34 @@ object Day23 {
             .toSet()
     }
 
+    // https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+    fun bronKerbosch(
+        lan: Lan,
+        clique: Set<String>,
+        someOfArg: Set<String>,
+        noneOfArg: Set<String>,
+    ): Sequence<Set<String>> =
+        sequence {
+            if (someOfArg.isEmpty() && noneOfArg.isEmpty()) yield(clique)
+
+            val someOf = someOfArg.toMutableSet()
+            val noneOf = noneOfArg.toMutableSet()
+
+            while (someOf.isNotEmpty()) {
+                val v = someOf.take(1)[0]
+                yieldAll(bronKerbosch(lan, clique.plus(v), someOf.intersect(lan[v]!!), noneOf.intersect(lan[v]!!)))
+                someOf.remove(v)
+                noneOf.add(v)
+            }
+        }
+
     fun part1(input: String): Int {
         return tripletsWithT(parse(input)).size
+    }
+
+    fun part2(input: String): String {
+        val lan = parse(input)
+        val cliques = bronKerbosch(lan, emptySet<String>(), lan.keys, emptySet<String>())
+        return cliques.sortedBy { it.size }.last().sorted().joinToString(",")
     }
 }
--- src/test/kotlin/org/cserby/aoc2024/test/Day23Test.kt
+++ src/test/kotlin/org/cserby/aoc2024/test/Day23Test.kt
@@ -14,4 +14,14 @@ class Day23Test {
     fun part1() {
         assertEquals(1240, Day23.part1(Utils.readFile("/day23.input.txt")))
     }
+
+    @Test
+    fun part2example() {
+        assertEquals("co,de,ka,ta", Day23.part2(Utils.readFile("/day23.example.txt")))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals("am,aq,by,ge,gf,ie,mr,mt,rw,sn,te,yi,zb", Day23.part2(Utils.readFile("/day23.input.txt")))
+    }
 }
